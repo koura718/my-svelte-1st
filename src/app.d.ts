d3b5821ae067595ffdd40b0a2014611b60c8f8a0
koura718 <koura718@gmail.com>
2023-03-30 14:12:02 +0900

    1st commit 20230330-1411

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,25 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+/// <reference types="@sveltejs/kit" />
+/// <reference types="@sveltejs/adapter-cloudflare" />
+
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
+		interface Platform {
+			env?: {
+			  YOUR_KV_NAMESPACE: KVNamespace
+			  YOUR_DURABLE_OBJECT_NAMESPACE: DurableObjectNamespace
+			}
+		}
 	}
 }
 
-export {};
+// export {};
+export async function actions({ request, platform }) {
+	const x = platform.env.YOUR_DURABLE_OBJECT_NAMESPACE.idFromName('x');
+}

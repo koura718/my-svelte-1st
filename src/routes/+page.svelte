ab88a121a47566212ff07f675d9037a8aa81e91e
koura718 <koura718@gmail.com>
2023-04-02 22:27:52 +0900

    20230402-2227

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit 1</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="/admin">Admin Page</a> to read the documentation</p>
+<p>Visit <a href="/api/random-number">api: random-number </a> to read the documentation</p>
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to Administrator</h1>
+<p>Visit <a href="/">Home</a> to read the documentation</p>

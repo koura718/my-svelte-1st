0236e47fdf3a644e65fe3566ce1c49a7ec2539ea
koura718 <koura718@gmail.com>
2023-04-19 00:21:21 +0900

    20230419-0021

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <h1>Welcome to SvelteKit 1</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="/about">About Page</a> to read the documentation</p>
 <p>Visit <a href="/admin">Admin Page</a> to read the documentation</p>
 <p>Visit <a href="/api/random-number">api: random-number </a> to read the documentation</p>
 <p>Visit <a href="/admin/openai">OpenAI Page</a> to read the documentation</p>
